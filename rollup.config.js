fdf5790ed5d8eb81c0da401dab556265b203b73b
Emmanuel <meric.emmanuel@gmail.com>
2019-12-14 14:52:57 +0100

    added main module entry

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,6 @@ import commonjs from 'rollup-plugin-commonjs';
 import pkg from './package.json';
 
 export default [
-	// browser-friendly UMD build
 	{
 		input: 'src/main.js',
 		output: {
@@ -12,17 +11,17 @@ export default [
 			format: 'umd'
 		},
 		plugins: [
-			resolve(), // so Rollup can find `ms`
-			commonjs() // so Rollup can convert `ms` to an ES module
+			resolve(),
+			commonjs()
 		]
 	},
 
-	// ES module (for bundlers) build
 	{
 		input: 'src/main.js',
 		external: ['ms'],
 		output: [
-			{ file: pkg.module, format: 'es' }
+			{ file: pkg.module, format: 'es' },
+			{ file: pkg.main, format: 'cjs' }
 		]
 	}
 ];
